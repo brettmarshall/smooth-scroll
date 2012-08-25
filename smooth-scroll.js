9c97a6c813e0c83f2edb3786175aa7fed3b37bf5
Brett Marshall <graphfx21@gmail.com>
2012-08-25 09:23:05 -0700

    Fixed html ID bug and fully commented code.

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -11,15 +11,16 @@
 jQuery(document).ready(function($)	{
 
 	smoothScroll = {
-		
+		// using this method, you need to specify the element you want to trigger the animation, the landing spot for the animation, and the speed of the scroll
 		Manual: function(button, landing, speed)	{
 			
-			
+			// take the clickable element (a tag) and add #smoothScroll-landing to the href
 			button.attr('href', '#smoothScroll-landing');
-			
+			// the default location of the landing spot will be html if not specified.
 			landing = landing || $('html');
+			// assigning the id smoothScroll-landing to the landing element
 			landing.attr('id', 'smoothScroll-landing');
-			
+			// the default speed is 300 milliseconds
 			speed = speed || 300;
 			
 				// when the item is clicked
@@ -35,9 +36,10 @@ jQuery(document).ready(function($)	{
 					});
 					return false;
 				});
-		}, // end of scroll
+		}, // end of the smoothScroll.Manual object
 		
-		
+		// This method assumes you are using a button that simply take the user back to the top of the page,
+		// You just need to give your button or "a" tag a class of "smoothScroll-trigger"
 		AutoDetect: function(speed)	{
 			
 			speed = speed || 300;
@@ -48,20 +50,34 @@ jQuery(document).ready(function($)	{
 			var smooth_first_div = $('div').first();
 			var smooth_button = $('.smoothScroll-trigger');
 			
-						
-			if ( smooth_html.attr('id') != null )	{
+			// if you don't have an id for the html element			
+			if ( smooth_html.attr('id') == null )	{
 				
+				// assign the id of "smoothScroll-landing" to the html element	
 				smooth_html.attr('id', 'smoothScroll-landing');
-				
+			
+			
+			
+			// else if you don't have an id for the body element		
 			} else if ( smooth_body.attr('id') == null )	{
 				
+				// assign the id of "smoothScroll-landing" to the body element
 				smooth_body.attr('id', 'smoothScroll-landing');
-				
+			
+			
+			
+			// else if you don't have an id for the header element	
 			} else if ( smooth_header.attr('id') == null )	{
 				
+				// assign the id of "smoothScroll-landing" to the header element
 				smooth_header.attr('id', 'smoothScroll-landing');
 				
+				
+				
+			// else if you don't have an id for the first div element	
 			} else if ( smooth_first_div.attr('id') == null )	{
+				
+				// assign the id of "smoothScroll-landing" to the first div element
 				smooth_first_div.attr('id', 'smoothScroll-landing');
 				
 			}
@@ -82,8 +98,8 @@ jQuery(document).ready(function($)	{
 				});
 				
 										
-		} // end of autodetect object literal
+		} // end of smoothScroll.AutoDetect object
 		
 		
-	}; // end of smooth object literal
+	}; // end of smoothScroll object
 });
